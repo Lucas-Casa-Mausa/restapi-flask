77c695174308e1872fcd59f755b426cfccb9d892
Lucas <lucascasamausa000@gmail.com>
2025-04-09 16:23:50 -0300

    fix: adding 0.0.0.0 due to connection refuse

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Resource,Api
 
+
 app = Flask(__name__)
 api = Api(app)
+
 
 class Users(Resource):
     def get(self):
@@ -10,7 +12,7 @@ class Users(Resource):
 
 class User(Resource):
     def get(self):
-        return {"message":"teste"}
+        return {"message":"CPF"}
     
 
     
@@ -19,5 +21,5 @@ api.add_resource(Users,'/users')
 api.add_resource(User,'/user','/user/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port="8000",debug=True)
+    app.run(host="0.0.0.0",debug=True)
 
